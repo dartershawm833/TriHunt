c4ae2d18d98a348c30a287c71dd1171dbe93160f
Trilleo <gongsanle@gmail.com>
2026-03-16 23:55:58 +0800

    Update: 0.1.0 release

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "2.3.10"
+}
+
+group = "net.trilleo"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+repositories {
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+tasks.jar {
+    from(configurations.runtimeClasspath.get().map { zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.jar {
+    manifest {
+        attributes["paperweight-mappings-namespace"] = "spigot"
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        logger.info("Plugin enabled!")
+    }
+
+    override fun onDisable() {
+        logger.info("Plugin disabled!")
+    }
+}
